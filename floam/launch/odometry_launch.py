8f9c76a88686dc59876ee0cb217de0e7d3f86486
Giuseppe Gabriele Tarollo <beppe.tarollo@gmail.com>
2023-12-20 13:37:07 +0100

    Added launch files for remaining nodes

---
+++ floam/launch/odometry_launch.py
@@ -0,0 +1,24 @@
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch_ros.substitutions import FindPackageShare
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
+
+import ament_index_python.packages
+import os
+import yaml
+
+def generate_launch_description():
+    my_launch_description = LaunchDescription()
+
+    loam_odometry_node = Node(
+        package='floam',
+        executable='floam_odom_estimation_node',
+        name='loam_odometry_node',
+        #prefix=['qterminal -e gdb -ex run --args'],
+        #output='screen'
+        # parameters=[{'nome_param1': 'valore_param1', 'nome_param2': 'valore_param2'}]
+    )
+    my_launch_description.add_action(loam_odometry_node)
+    
+    return my_launch_description
