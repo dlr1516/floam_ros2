01a0f6630e284b672ab88e580482b112258d9ddb
dlr1516 <dario.lodirizzini@unipr.it>
2024-02-09 17:26:28 +0100

    added ROS2 package cloud_file_publisher

--- cloud_file_publisher/src/cloud_publisher.cpp
+++ cloud_file_publisher/src/cloud_publisher.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <deque> 
+#include <chrono>
 
 #include <pcl_conversions/pcl_conversions.h>  // deprecated?
 #include <rclcpp/rclcpp.hpp>
@@ -9,7 +11,7 @@
 
 #include <glob.h>
 
-void glob(const std::string& globPath, std::vector<std::string>& matchingFiles) {
+void glob(const std::string& globPath, std::deque<std::string>& matchingFiles) {
     glob_t glob_result;
     matchingFiles.clear();
     std::cout << "global path: \"" << globPath << "\"" << std::endl;
@@ -41,52 +43,62 @@ class CloudPublisherNode : public rclcpp::Node {
         this->get_parameter("cloud_topic", cloud_topic_);
         this->declare_parameter("cloud_file_glob", "");
         this->get_parameter("cloud_file_glob", cloud_file_glob);
+        this->declare_parameter("frame_id", "laser_link");
+        this->get_parameter("frame_id", frame_id_);
+        this->declare_parameter("pub_period_ms", "100.0");
+        this->get_parameter("pub_period_ms", pub_period_ms_);
 
         // Initializes the publisher.
         cloudPub_ = this->create_publisher<sensor_msgs::msg::PointCloud2>(
             cloud_topic_, 10);
-            
+          
+        // Collect the list of all the files fitting with the glob  
         glob(cloud_file_glob, cloud_files_); 
-        RCLCPP_INFO_STREAM(this->get_logger(), "cloud_file_glob: \"" << cloud_file_glob << "\": found " << cloud_files_);  
+        RCLCPP_INFO_STREAM(this->get_logger(), "cloud_file_glob: \"" << cloud_file_glob << "\": found " << cloud_files_.size());
         
+        // Initializes the point cloud publisher
+        cloudPub_ =
+            this->create_publisher<sensor_msgs::msg::PointCloud2>(cloud_topic_, 10);  
+            
+        auto period = std::chrono::duration_cast<std::chrono::steady_clock::duration>(
+                              std::chrono::duration<float>(pub_period_ms_));
         timer_ = this->create_wall_timer(
-            50ms, std::bind(&AvoidObstacleNode::commandPeriodic, this));
+            period, std::bind(&CloudPublisherNode::publishPeriodic, this));
     }
+    
 
-//    /**
-//     * Callback to handle new laser point cloud messages.
-//     */
-//    void onPointCloud(sensor_msgs::msg::PointCloud2::SharedPtr msg) {
-//        pcl::PointCloud<pcl::PointXYZI>::Ptr cloudIn(
-//            new pcl::PointCloud<pcl::PointXYZI>);
+    void publishPeriodic() {
+      if (cloud_files_.empty()) {
+        RCLCPP_DEBUG_STREAM(this->get_logger(), "list of cloud files finished: no more cloud to be published");
+        return; 
+      }
+      
+      std::string cloudFilename = cloud_files_.front();
+      cloud_files_.pop_front();
+    
+      pcl::PointCloud<pcl::PointXYZI>::Ptr points(new pcl::PointCloud<pcl::PointXYZI>);
+      RCLCPP_DEBUG_STREAM(this->get_logger(), "reading file " << cloudFilename);
+      if (pcl::io::loadPCDFile<pcl::PointXYZI> (cloudFilename.c_str(), *points) == -1) {
+         RCLCPP_ERROR_STREAM(this->get_logger(), "Could not read file: " << cloudFilename);
+         return;
+       }
+      
 
-//        // pcl::fromROSMsg() is deprecated, but it is the simpler API to convert
-//        // from sensor_msgs::msg::PointCloud2 to pcl::PointCloud<> Warning:
-//        // installing external PCL and ROS2 in Ubuntu 22.04 you may have
-//        // duplicate versions:
-//        // - /usr/include/pcl_conversions: uses ROS1 sensor_msgs::PointCloud2
-//        //   void fromROSMsg(const sensor_msgs::PointCloud2 &cloud,
-//        //   pcl::PointCloud<T> &pcl_cloud);
-//        // - /opt/ros/humble/include/pcl_conversions: uses ROS2 message
-//        //   void fromROSMsg(const sensor_msgs::msg::PointCloud2 &cloud,
-//        //   pcl::PointCloud<T> &pcl_cloud)
-//        pcl::fromROSMsg(
-//            *msg,
-//            *cloudIn);  // possibly deprecated now, but more straightforward
-//        // We use: void toPCL(const sensor_msgs::msg::PointCloud2 &pc2,
-//        // pcl::PCLPointCloud2 &pcl_pc2)
-//        //   	pcl::PCLPointCloud2::Ptr cloudPcl(new pcl::PCLPointCloud2);
-//        //        pcl_conversions::toPCL(*msg, *cloudPcl);
-
-//        std::cout << "received point cloud message with "
-//                  << cloudIn->points.size() << " points" << std::endl;
-//    }
+       //workaround for the PCL headers... http://wiki.ros.org/hydro/Migration#PCL
+       sensor_msgs::msg::PointCloud2::SharedPtr cloudMsg(new sensor_msgs::msg::PointCloud2);
+       pcl::toROSMsg(*points, *cloudMsg);
+       cloudMsg->header.frame_id = frame_id_; //ros::this_node::getName();
+       cloudMsg->header.stamp = this->now();
+       cloudPub_->publish(*cloudMsg);
+    }
 
    private:
     rclcpp::Publisher<sensor_msgs::msg::PointCloud2>::SharedPtr cloudPub_;
     rclcpp::TimerBase::SharedPtr timer_;
     std::string cloud_topic_;
-    std::vector<std::string> cloud_files_;
+    std::string frame_id_;
+    float pub_period_ms_;
+    std::deque<std::string> cloud_files_;
 };
 
 int main(int argc, char** argv) {
